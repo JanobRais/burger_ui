cb732c5f4e0fe6f00b059d45fe5f9d5b8930bc40
JanobRais <Nuriddinasqarov98>
2022-08-20 12:44:49 +0500

    Burger UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:burger_ui/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -36,38 +37,83 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Image.asset(
-                "assets/burger.jpg",
-                fit: BoxFit.cover,
-              ),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              "assets/burger.jpg",
+              fit: BoxFit.cover,
             ),
-            Column(
-              children: [
-                SizedBox(
-                  height: 60,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 60,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Mazali \nBurger",
+                    style: GoogleFonts.raleway(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 1.7,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  "Ajoyib ta'm \nTez yetkazib berish",
+                  style: GoogleFonts.raleway(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 43),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Mazali \nBurger",
-                      style: GoogleFonts.raleway(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange, // background
+                      onPrimary: Colors.white,
+
+
+                      // foreground
                     ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_)=>HomePage()));
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                            child: Text(
+                          "Kirish",
+                          style: GoogleFonts.raleway(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        )),
+                        SizedBox(height: 55,)
+                      ],
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40, left: 30),
+                child: Row(children: [
+                  Text("akkauntingiz yo'qmi ?   ", style: TextStyle(color: Colors.white),),
+                  Text("parolni unutdingizmi ?", style: TextStyle(color: Colors.white),)
+                ],),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
